c876b964e4725e3f45ed5702c312f7a3f49fa90a
Xinyuan <42595900+xinyuan1995@users.noreply.github.com>
2018-09-08 02:52:29 -0400

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,19 @@
+import numpy as np
+import scipy.interpolate
+import matplotlib.pyplot as plt
+import pylab
+
+x_parabola = np.array([-1, 0, 1]) # array for x
+y_parabola = np.array([-0.2672, 0, 0.1638]) # array for y
+plt.figure()
+u = plt.plot(x_parabola,y_parabola,'ro') # plot the points
+t = np.linspace(0, 1, len(x_parabola)) # parameter t to parametrize x and y
+pxLagrange = scipy.interpolate.lagrange(t, x_parabola) # X(T)
+pyLagrange = scipy.interpolate.lagrange(t, y_parabola) # Y(T)
+n = 100
+ts = np.linspace(t[0],t[-1],n)
+xLagrange = pxLagrange(ts) # lagrange x coordinates
+yLagrange = pyLagrange(ts) # lagrange y coordinates
+plt.plot(xLagrange, yLagrange,'b-',label = "Polynomial")
+plt.show()
+pylab.savefig('Lagrangian_polynomial_for_N=3.png')
