92a4842ea4f6bca1e98fc69f590d52ba38ab10b6
Xinyuan <42595900+xinyuan1995@users.noreply.github.com>
2018-09-08 03:24:30 -0400

    Add files via upload

--- HW1.py
+++ HW1.py
@@ -1,19 +1,20 @@
 import numpy as np
 import scipy.interpolate
 import matplotlib.pyplot as plt
-import pylab
 
-x_parabola = np.array([-1, 0, 1]) # array for x
-y_parabola = np.array([-0.2672, 0, 0.1638]) # array for y
+x = np.array([-1, 0, 1])
+y = np.array([-0.2672, 0, 0.1638])
 plt.figure()
-u = plt.plot(x_parabola,y_parabola,'ro') # plot the points
-t = np.linspace(0, 1, len(x_parabola)) # parameter t to parametrize x and y
-pxLagrange = scipy.interpolate.lagrange(t, x_parabola) # X(T)
-pyLagrange = scipy.interpolate.lagrange(t, y_parabola) # Y(T)
-n = 100
-ts = np.linspace(t[0],t[-1],n)
-xLagrange = pxLagrange(ts) # lagrange x coordinates
-yLagrange = pyLagrange(ts) # lagrange y coordinates
-plt.plot(xLagrange, yLagrange,'b-',label = "Polynomial")
-plt.show()
-pylab.savefig('Lagrangian_polynomial_for_N=3.png')
+u = plt.plot(x,y,'ro')
+t = np.linspace(0, 1, len(x))
+x_interpolation = scipy.interpolate.lagrange(t, x)
+y_interpolation = scipy.interpolate.lagrange(t, y)
+#t_lagrange = np.linspace(t[0],t[-1],200)
+t_lagrange = np.linspace(0,len(x),200)
+x_lagrange = x_interpolation(t_lagrange)
+y_lagrange = y_interpolation(t_lagrange)
+plt.plot(x_lagrange, y_lagrange)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Lagrangian polynomial for N=3')
+plt.savefig('Lagrangian polynomial for N=3.png')
