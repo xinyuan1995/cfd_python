ead3e5ea0027bda6b4e20547560a64464f4cf87b
Xinyuan <42595900+xinyuan1995@users.noreply.github.com>
2018-09-08 05:59:14 -0400

    Add files via upload

--- HW1.py
+++ HW1.py
@@ -2,19 +2,39 @@ import numpy as np
 import scipy.interpolate
 import matplotlib.pyplot as plt
 
-x = np.array([-1, 0, 1])
-y = np.array([-0.2672, 0, 0.1638])
-plt.figure()
-u = plt.plot(x,y,'ro')
-t = np.linspace(0, 1, len(x))
-x_interpolation = scipy.interpolate.lagrange(t, x)
-y_interpolation = scipy.interpolate.lagrange(t, y)
-#t_lagrange = np.linspace(t[0],t[-1],200)
-t_lagrange = np.linspace(0,len(x),200)
-x_lagrange = x_interpolation(t_lagrange)
-y_lagrange = y_interpolation(t_lagrange)
-plt.plot(x_lagrange, y_lagrange)
+# # Given points for interpolation
+# x = np.array([-1, 0, 1])
+# y = np.array([-0.2672, 0, 0.1638])
+# u = plt.plot(x, y, 'ro')
+# # Obtain the Lagrange polynomial of the given points
+# Lagrangian_poly = scipy.interpolate.lagrange(x, y)
+# print(Lagrangian_poly)  # check the polynomial comment out when necessary
+# # Create the x axis line space as t
+# t = np.linspace(-1, 1, 100)
+# # Plot the polynomial
+# ax = plt.plot(t, Lagrangian_poly(t))
+# # Labels and title
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.title('Lagrangian polynomial for N=3')
+# plt.savefig('Lagrangian polynomial for N=3.png')
+
+
+x = np.zeros(41)
+y = np.zeros(41)
+for i in range(-20, 21):
+    y[i+20] = np.tanh(i)*np.sin(5*i+1.5)
+    x[i+20] = i
+u = plt.plot(x, y, 'ro')
+# Obtain the Lagrange polynomial of the given points
+Lagrangian_poly = scipy.interpolate.lagrange(x, y)
+print(Lagrangian_poly)  # check the polynomial comment out when necessary
+# Create the x axis line space as t
+t = np.linspace(-1, 1, 100)
+# Plot the polynomial
+ax = plt.plot(t, Lagrangian_poly(t))
+# Labels and title
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Lagrangian polynomial for N=3')
-plt.savefig('Lagrangian polynomial for N=3.png')
+plt.title('Lagrangian polynomial for N=41')
+plt.savefig('Lagrangian polynomial for N=41.png')
